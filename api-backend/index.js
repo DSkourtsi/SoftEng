066f547604905f1914479dd4a5136f9c030d709f
ntua-el18044 <el18044@mail.ntua.gr>
2023-01-20 21:41:42 +0200

    Update index.js

--- api-backend/index.js
+++ api-backend/index.js
@@ -1,19 +1,16 @@
 const express = require("express")
-const port = 9103
-const app = express()
-
 const mongoose = require('mongoose');
 const mongoString = "mongodb+srv://softeng22-99:softeng22@softeng99.uap3fhl.mongodb.net/test";
+const port = 9103
+
+const app = express()
 
 mongoose.set('strictQuery', false);
-
 mongoose.connect(mongoString);
 const database = mongoose.connection;
-
 database.on('error', (error) => {
     console.log(error)
 })
-
 database.once('connected', () => {
     console.log('Database Connected');
 })
