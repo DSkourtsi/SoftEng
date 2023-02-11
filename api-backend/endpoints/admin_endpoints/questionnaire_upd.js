bc51d16c1fe92ccab8cf718b623a28c0fb790237
Cleopatra Dimaraki <94308377+ntua-el18155@users.noreply.github.com>
2023-02-11 22:44:29 +0200

    Add files via upload

--- api-backend/endpoints/admin_endpoints/questionnaire_upd.js
+++ api-backend/endpoints/admin_endpoints/questionnaire_upd.js
@@ -1,43 +1,109 @@
 const mysql = require("mysql");
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
-const fs = require("fs");
+const fs = require("fs")
 
-function questionnaire_upd(req, res){
 
-    fs.readFile()
+fs.mkdirSync('uploads', { recursive: true });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${file.fieldname}-${Date.now()}.json`);
+    },
+  });
+  
+const upload = multer({ storage });
+  
+const db_string = {
+    host: "localhost",
+    user: "root",
+    password: "intelliq",
+    database: 'intelliq_database'
+};
 
-    const db_string = {
-        host: "localhost",
-        user: "root",
-        password: "intelliq",
-        database: 'intelliq_database'
-    };
+let db_con = mysql.createConnection(db_string);
 
-    let db_con = mysql.createConnection(db_string);
+db_con.connect((err) => {
+    if (err) {
+      console.error('Error connecting to database: ' + err.stack);
+      return;
+    }
+    console.log('Connected to database as id ' + db_con.threadId);
+  });
 
-    db_con.connect((err) => {
-        if (err) {
-            console.log("Database Connection Failed !!!", err);
-            var Json = {
-                status: "failed",
-                dbconnection: db_string
-            };
-            res.status(500).send(Json);
-            return;
-        }
-        else {
-            console.log("Connected to Database");
-            var Json = {
-                status: "OK",
-                dbconnection: db_string
-            };
-            res.status(200).send(Json);
-            return;
-        }
+const questionnaire_upload = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file.path, (err, data) => {
+            if (err) {
+                reject({ error: 'Error reading file.'})
+                return;
+            }
+            try {               
+                const jsonData = JSON.parse(data);                
+                db_con.query(
+                    `INSERT INTO questionnaire SET ?`,
+                    { questionnaireID: jsonData.questionnaireID, questionnaireTitle: jsonData.questionnaireTitle, keywords: JSON.stringify(jsonData.keywords) },
+                    (error, results) => {
+                        if (error) {
+                            console.error(error);
+                        }
+                        else {
+                            jsonData.questions.forEach(question => {
+                                db_con.query(
+                                    `INSERT INTO question SET ?`,
+                                    { questionnaireID: jsonData.questionnaireID, qID: question.qID, qtext: question.qtext, required: question.required, type: question.type },
+                                    (error, results) => {
+                                        if (error) {
+                                            console.error(error);
+                                        }
+                                        else {
+                                            question.options.forEach(options => {
+                                                let nextqID = options.nextqID;
+                                                if (nextqID === "-") {
+                                                    nextqID = null;
+                                                }
+                                                db_con.query(
+                                                    `INSERT INTO options SET ?`,
+                                                    { qID: question.qID, optID: options.optID, opttxt: options.opttxt, nextqID: nextqID },
+                                                    (error, results) => {
+                                                        if (error) {
+                                                            console.error(error);
+                                                        }
+                                                        else {
+                                                            console.log(results);
+                                                        }
+                                                    }
+                                                );
+                                            });
+                                        }
+                                    }
+                                );
+                            });
+                        }
+                    }
+                );
+                resolve(jsonData);
+            }   catch (e) {
+                    reject({ error: 'Invalid JSON file.'});
+            }
+        });
     });
-}
+};
 
-router.get("/admin/healthcheck", healthcheck);
+router.use(upload.single('file'));
+
+router.post("/admin/questionnaire_upd", (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: 'File is required.' });
+    }    
+    questionnaire_upload(req.file).then((result) => {
+        res.json(result);
+    }).catch((error) => {
+        res.status(400).json(error);
+    });
+});
 
 module.exports = router;
