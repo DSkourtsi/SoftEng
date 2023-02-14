4bcb01d1c4faf8b42c7f0c38affdefb252a107ba
Cleopatra Dimaraki <94308377+ntua-el18155@users.noreply.github.com>
2023-02-14 15:33:23 +0200

    Add files via upload

---
+++ cli/scopes/admin/questionnaire_upd.js
@@ -0,0 +1,20 @@
+const env = require("../../env.js");
+const fs = require('fs');
+const FormData = require('form-data');
+const path = require('path');
+
+function questionnaire_updFunction(source) {
+    const filePath = path.join(process.cwd(), '..', 'data', source);
+    const fileStream = fs.createReadStream(filePath);
+
+    const formData = new FormData();
+    formData.append('file', fileStream, { filename: 'file.txt' });
+    
+    env.instance.post(`${env.baseURL}/admin/questionnaire_upd`, formData, {
+        headers: formData.getHeaders(),
+    }).then((res) => {
+        console.log(res.data);
+    });
+}
+
+module.exports = { questionnaire_updFunction };
